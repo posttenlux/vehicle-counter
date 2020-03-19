b31fa8ee26288ccb04dc506ca5d21362d7249dca
Alex Stevens <post.ten.lux@gmail.com>
2020-03-19 17:24:17 -0400

    Added coco.names file. Updated .gitignore to allow coco.names file. Updated download script to create yolo-coco dir if it doesn't already exist

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
+mkdir -p yolo-coco
 curl -L https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg -o yolo-coco/yolov3.cfg
 curl -L https://pjreddie.com/media/files/yolov3.weights -o yolo-coco/yolov3.weights
